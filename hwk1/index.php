e75532af5a6462f86b4513dd3eb8bc4256ef258f
Luis Naranjo <luisnaranjo733@gmail.com>
2017-01-11 10:59:57 -0800

    add select * from nba stats

--- hwk1/index.php
+++ hwk1/index.php
@@ -46,8 +46,6 @@
       <button type="submit" class="btn btn-primary">Submit</button>
     </form>
 
-    <?php echo '<p>Hello World</p>'; ?> 
-
   </div>
 
   <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
@@ -66,7 +64,6 @@
 
 
 <?php
-
   /* Connect to MySQL and select the database. */
   $connection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD);
 
@@ -74,20 +71,10 @@
 
   $database = mysqli_select_db($connection, DB_DATABASE);
 
-  /* Ensure that the Employees table exists. */
-  VerifyEmployeesTable($connection, DB_DATABASE); 
-
-  /* If input fields are populated, add a row to the Employees table. */
-  $employee_name = htmlentities($_POST['Name']);
-  $employee_address = htmlentities($_POST['Address']);
-
-  if (strlen($employee_name) || strlen($employee_address)) {
-    AddEmployee($connection, $employee_name, $employee_address);
-  }
 ?>
 
 <!-- Input form -->
-<form action="<?PHP echo $_SERVER['SCRIPT_NAME'] ?>" method="POST">
+<!--<form action="<?PHP echo $_SERVER['SCRIPT_NAME'] ?>" method="POST">
   <table border="0">
     <tr>
       <td>Name</td>
@@ -105,19 +92,21 @@
       </td>
     </tr>
   </table>
-</form>
+</form>-->
 
 <!-- Display table data. -->
 <table border="1" cellpadding="2" cellspacing="2">
   <tr>
-    <td>ID</td>
     <td>Name</td>
-    <td>Address</td>
+    <td>Team</td>
+    <td>GP</td>
   </tr>
 
 <?php
+// user input
+$searchQuery = $_GET["searchQuery"];
 
-$result = mysqli_query($connection, "SELECT * FROM Employees"); 
+$result = mysqli_query($connection, "SELECT * FROM playerStats"); 
 
 while($query_data = mysqli_fetch_row($result)) {
   echo "<tr>";
@@ -141,45 +130,3 @@ while($query_data = mysqli_fetch_row($result)) {
 </body>
 </html>
 
-
-<?php
-
-/* Add an employee to the table. */
-function AddEmployee($connection, $name, $address) {
-   $n = mysqli_real_escape_string($connection, $name);
-   $a = mysqli_real_escape_string($connection, $address);
-
-   $query = "INSERT INTO `Employees` (`Name`, `Address`) VALUES ('$n', '$a');";
-
-   if(!mysqli_query($connection, $query)) echo("<p>Error adding employee data.</p>");
-}
-
-/* Check whether the table exists and, if not, create it. */
-function VerifyEmployeesTable($connection, $dbName) {
-  if(!TableExists("Employees", $connection, $dbName)) 
-  { 
-     $query = "CREATE TABLE `Employees` (
-         `ID` int(11) NOT NULL AUTO_INCREMENT,
-         `Name` varchar(45) DEFAULT NULL,
-         `Address` varchar(90) DEFAULT NULL,
-         PRIMARY KEY (`ID`),
-         UNIQUE KEY `ID_UNIQUE` (`ID`)
-       ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1";
-
-     if(!mysqli_query($connection, $query)) echo("<p>Error creating table.</p>");
-  }
-}
-
-/* Check for the existence of a table. */
-function TableExists($tableName, $connection, $dbName) {
-  $t = mysqli_real_escape_string($connection, $tableName);
-  $d = mysqli_real_escape_string($connection, $dbName);
-
-  $checktable = mysqli_query($connection, 
-      "SELECT TABLE_NAME FROM information_schema.TABLES WHERE TABLE_NAME = '$t' AND TABLE_SCHEMA = '$d'");
-
-  if(mysqli_num_rows($checktable) > 0) return true;
-
-  return false;
-}
-?>
