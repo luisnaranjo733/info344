768c0f1915ced4283b8594a1c9c8a8e47ddb6edf
Luis Naranjo <luisnaranjo733@gmail.com>
2017-01-11 23:32:59 -0800

    restructure css

--- hwk1/index.php
+++ hwk1/index.php
@@ -39,7 +39,7 @@ try {
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
     crossorigin="anonymous">
 
-  <link rel="stylesheet" href="./index.css">
+  <link rel="stylesheet" href="css/index.css">
 
   <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
