0a7fa1720c4ec3f15741190dc9a08f4f76f08181
Luis Naranjo <luisnaranjo733@gmail.com>
2017-03-03 22:10:11 -0800

    refactory sum code

--- hwk1/api.php
+++ hwk1/api.php
@@ -14,25 +14,20 @@ if (!isset($_GET['searchQuery'])) {
 
 try {
     $searchQuery = $_GET['searchQuery'];
-    $executeQuery = $searchQuery !== '%%';
-    if ($executeQuery) { // if query is not empty string
-      $conn = new PDO('mysql:host=tutorial-db-instance.cejtkzfmojjc.us-west-2.rds.amazonaws.com:3306;dbname=nba', $username, $password);
-      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);    
-      
-      $stmt = $conn->prepare('SELECT * FROM playerStats WHERE `Name` LIKE :name LIMIT 1');
-      $stmt->bindParam(':name', $searchQuery, PDO::PARAM_STR);
-      $stmt->execute(); // execute SQL query
-    }
+    $conn = new PDO('mysql:host=tutorial-db-instance.cejtkzfmojjc.us-west-2.rds.amazonaws.com:3306;dbname=nba', $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);    
+    
+    $stmt = $conn->prepare('SELECT * FROM playerStats WHERE `Name` LIKE :name LIMIT 1');
+    $stmt->bindParam(':name', $searchQuery, PDO::PARAM_STR);
+    $stmt->execute(); // execute SQL query
 
     
     $players = array();
-    if ($executeQuery) {
-      while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-          $player = new Player(
-            $row['Name'], $row['Team'], $row['GP'], $row['FTPct'], $row['PPG'], $row['3PTPct']
-          );
-          array_push($players, $player);
-      }
+    while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        $player = new Player(
+          $row['Name'], $row['Team'], $row['GP'], $row['FTPct'], $row['PPG'], $row['3PTPct']
+        );
+        array_push($players, $player);
     }
 
     if(isset($_GET['callback']))
